d1e35db943d2d0ec360ffed4e887ec0747601339
Rashotech <rashotech@gmail.com>
2021-07-25 11:31:34 +0100

    migration files

---
+++ database/migrations/2021_07_24_221940_add_image_to_car_models_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddImageToCarModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('car_models', function (Blueprint $table) {
+            $table->string('image_path');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('car_models', function (Blueprint $table) {
+            //
+        });
+    }
+}
